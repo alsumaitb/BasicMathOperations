4be67cc98f4517811590882da1fbb6602b2feec2
alsumait.b <alsumait.b@northeastern.edu>
2024-03-11 21:36:35 -0400

    Created main function outside class

--- Assignment05_BasicMathOperations_Alsumait.py
+++ Assignment05_BasicMathOperations_Alsumait.py
@@ -55,6 +55,65 @@ class BasicMathOperations:
     def typeofArgument(self, argument):
         return type(argument)
     
+def main():
+  
+    bmo = BasicMathOperations()
+    while True:
+    
+        print("1. Greet User\n2. Add number\n3. Perform Operation\n4. Square number\n5. Factorial\n6. Counting\n7. Calculate Hypotenuse\n8. Rectangle Area\n9. Power of Number\n10. Type of Argument\n11. Nothing")
+        user_choice = input("Pick the task number you would like to execute: ")
+       
+        if user_choice == "1":
+            firstname = input("Enter first name: ")
+            lastname = input("Enter last name: ")
+            bmo.greetUser(firstname, lastname)
+       
+        elif user_choice == "2":
+            bmo.addNumbers()
+        
+        elif user_choice == "3":
+            num1 = int(input("Enter first number: "))
+            num2 = int(input("Enter second number: "))
+            operator = input("Enter math operator(+,-,*,/): ")
+            bmo.performOperation(num1, num2, operator)
+       
+        elif user_choice == "4":
+            num = int(input("Enter number: "))
+            bmo.squareNumber(num)
+        
+        elif user_choice == "5":
+            num = int(input("Enter number: "))
+            bmo.factorial(num)
+       
+        elif user_choice == "6":
+            bmo.counting()
+        
+        elif user_choice == "7":
+            a = int(input("Enter a of right triangle: "))
+            b = int(input("Enter b of right triangle: "))
+            bmo.calculateHypotenuse(a, b)
+        
+        elif user_choice == "8":
+            width = int(input("Enter width of rectangle: "))
+            height = int(input("Enter height of rectangle: "))
+            bmo.areaofRectangle(width, height)
+        
+        elif user_choice == "9":
+            base = int(input("Enter base: "))
+            exponent = int(input("Enter exponent: "))
+            bmo.powerofNumber(base, exponent)
+        
+        elif user_choice == "10":
+            argument = input("Enter any data type: ")
+            bmo.typeofArgument(argument)
+       
+        elif user_choice == "11":
+            break
+       
+        else:
+            print("Invalid choice")
+            
+        
    
         
         
