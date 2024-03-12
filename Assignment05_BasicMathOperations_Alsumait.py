389f37adfb3666392492e9603e0eeb66cc47b928
alsumait.b <alsumait.b@northeastern.edu>
2024-03-11 20:44:49 -0400

    added greet user and add numbers method to class

--- Assignment05_BasicMathOperations_Alsumait.py
+++ Assignment05_BasicMathOperations_Alsumait.py
@@ -1,1 +1,14 @@
 # Assignment 05, BasicMathOperations
+
+class BasicMathOperations:
+    
+    def greetUser(self, firstname, lastname):
+        print(f"Hello {firstname} {lastname} !")
+        
+    def addNumbers(self):
+        user_prompt_1 = int(input("Enter first number: "))
+        user_prompt_2 = int(input("Enter second number: "))
+        
+        return user_prompt_1 + user_prompt_2
+    
+    
